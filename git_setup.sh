8136fb58cd51f517da03523e9b087e1f16ddce14
sandy1618 <sandeepnayak1.618@gmail.com>
2024-08-30 18:25:42 +0900

    [Mod] Adding checks for repos existing in git_setup.sh

--- git_setup.sh
+++ git_setup.sh
@@ -12,18 +12,27 @@ sudo apt install git -y
 REPO_DIR=~/Documents/Repository
 
 if [ -d "$REPO_DIR" ]; then
-    echo "Directory $REPO_DIR already exists. Changing to $REPO_DIR..."
+    echo "Directory $REPO_DIR already exists."
 else
     echo "Creating repository directory at $REPO_DIR..."
     mkdir -p $REPO_DIR
 fi
 
-echo "Cloning SimpleAutomation repository into $REPO_DIR..."
-git clone https://github.com/sandy1618/SimpleAutomation.git $REPO_DIR/SimpleAutomation
+# Define the repository URL and the target directory
+REPO_URL="https://github.com/sandy1618/SimpleAutomation.git"
+TARGET_DIR="$REPO_DIR/SimpleAutomation"
+
+# Check if the repository already exists
+if [ -d "$TARGET_DIR" ]; then
+    echo "Repository SimpleAutomation already exists at $TARGET_DIR. Skipping clone."
+else
+    echo "Cloning SimpleAutomation repository into $REPO_DIR..."
+    git clone $REPO_URL $TARGET_DIR
+fi
 
 # Make all .sh files in the SimpleAutomation repository executable
-echo "Making all .sh files in $REPO_DIR/SimpleAutomation executable..."
-find $REPO_DIR/SimpleAutomation -type f -name "*.sh" -exec chmod +x {} \;
+echo "Making all .sh files in $TARGET_DIR executable..."
+find $TARGET_DIR -type f -name "*.sh" -exec chmod +x {} \;
 
 # Clean up
 echo "Cleaning up..."
