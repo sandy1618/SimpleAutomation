6e80361651392f081e13e5b4555c0cfc4fa0f7c1
sandy1618 <sandeepnayak1.618@gmail.com>
2024-08-30 18:23:45 +0900

    [Mod] Making scripts executable in github simpleAutomation

--- git_setup.sh
+++ git_setup.sh
@@ -21,6 +21,10 @@ fi
 echo "Cloning SimpleAutomation repository into $REPO_DIR..."
 git clone https://github.com/sandy1618/SimpleAutomation.git $REPO_DIR/SimpleAutomation
 
+# Make all .sh files in the SimpleAutomation repository executable
+echo "Making all .sh files in $REPO_DIR/SimpleAutomation executable..."
+find $REPO_DIR/SimpleAutomation -type f -name "*.sh" -exec chmod +x {} \;
+
 # Clean up
 echo "Cleaning up..."
 sudo apt autoremove -y
